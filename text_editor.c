0f1be3ec6c8afa0523d23a48756b54edac5426a6
abc <abc@example.com>
2018-09-24 11:15:22 +0530

    printing from buffer

--- text_editor.c
+++ text_editor.c
@@ -7,39 +7,55 @@
 
 #define CTRL(c) ((c) & 037)
 
-char buffer[10];
+char *buffer;
 static wint_t key;
-int main()
-{
-	static unsigned int width, height;
-	wint_t key;
-	int buf_idx = 0,x=0,y=0;
+unsigned int buf_idx = 0,buf_size=10;
+static unsigned int width, height;
 
-	initscr(); raw(); noecho(); nonl(); keypad(stdscr, TRUE);
-	setlocale(LC_ALL, ""); 
-   	getmaxyx(stdscr, height, width);
-	while(1)
-	{
-		wget_wch(stdscr,&key);
-	    buffer[buf_idx++] = key;
-		//redrawwin(stdscr); 			
-			
-		if(key==CTRL('m'))
+static void redraw_screen()
+{
+	int x=0,y=0;
+	for(int i=0;i<buf_idx;i++)
+    {
+		if(buffer[i]==CTRL('m'))
 		{
 			y = (y+1)%height;
 			x = 0;
 			mvprintw(y,x,"");
 			continue;
 		}
-		if(key==CTRL('q'))
-			break;
-		//printable key
-		mvprintw(y,x,"%c",key);
+		mvprintw(y,x,"%c",buffer[i]);
 		if(x++ >= width)
 		{
 			x = 0;
 			y = (y+1)%height;
 		}
+	}		
+}
+
+int main()
+{
+	wint_t key;
+	int x=0,y=0;
+
+	initscr(); raw(); noecho(); nonl(); keypad(stdscr, TRUE);
+	setlocale(LC_ALL, ""); 
+   	getmaxyx(stdscr, height, width);
+	buffer = (char*) malloc(buf_size);
+	
+	while(1)
+	{
+		wget_wch(stdscr,&key);
+		if(key==CTRL('q'))
+			break;
+		//printable key
+		if(buf_idx == buf_size)
+		{
+			buffer = realloc(buffer,buf_size+10);
+			buf_size += 10;
+		}
+		buffer[buf_idx++] = key;
+		redraw_screen();
 	}
 	
 	noraw(); endwin(); return 0;
