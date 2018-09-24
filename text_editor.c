2c9d4218a178f6d2b9ebcf21bfedb0bc7cbf1cbf
abc <abc@example.com>
2018-09-24 22:54:51 +0530

    added a central data structure for lines, removed global buffer

--- text_editor.c
+++ text_editor.c
@@ -3,35 +3,67 @@
 #include<stdlib.h>
 #include<wchar.h>
 #include<locale.h>
+#include<string.h>
 //#include<wchar.h>
 
 #define CTRL(c) ((c) & 037)
+#define BUF_INCR 10
 
-char *buffer;
+typedef struct line 
+{
+	char *text;
+	int usize;
+	int asize;
+	struct line *next,*prev;	
+}line_t;
+
+line_t *first;
+line_t *curr;
 static wint_t key;
-unsigned int buf_idx = 0,buf_size=10;
 static unsigned int width, height;
 
 static void redraw_screen()
 {
-	int x=0,y=0;
 	clear();
-	for(int i=0;i<buf_idx;i++)
-    {
-		if(buffer[i]==CTRL('m'))
-		{
-			y = (y+1)%height;
-			x = 0;
-			mvprintw(y,x,"");
-			continue;
-		}
-		mvprintw(y,x,"%c",buffer[i]);
-		if(x++ >= width)
-		{
-			x = 0;
-			y = (y+1)%height;
-		}
-	}		
+	int y = 0;
+	line_t *line = first;
+	while(line)
+	{
+		mvprintw(y++,0,"%s",line->text);
+		line=line->next;
+	}
+}
+
+static line_t* new_line()
+{
+	line_t *new = (line_t*) malloc(sizeof(line_t));
+	new->usize = 0;
+	new->asize = BUF_INCR; 
+	new->text = (char*)malloc(new->asize);
+	new->next = new->prev = NULL;
+	return new;
+}
+
+static void insert_char(char c)
+{
+	if(curr->usize == curr->asize)
+	{
+		curr->asize +=BUF_INCR;
+		curr->text = realloc(curr->text,curr->asize);
+	}
+	curr->text[curr->usize++]=c;
+}
+
+static void  handle_backspace()
+{
+	line_t *line = curr;
+	while(line && line->usize==0)
+		line = line->prev;
+	if(line)
+	{
+		line->text[line->usize]='\0';
+		line->usize--;
+	}
 }
 
 int main()
@@ -42,7 +74,8 @@ int main()
 	initscr(); raw(); noecho(); nonl(); keypad(stdscr, TRUE);
 	setlocale(LC_ALL, ""); 
    	getmaxyx(stdscr, height, width);
-	buffer = (char*) malloc(buf_size);
+	first = new_line();
+	curr=first;
 	
 	while(1)
 	{
@@ -51,18 +84,22 @@ int main()
 			break;
 		if(key==127)//KEY_BACKSPACE)
 		{
-			//mvprintw(0,0,"backspace");
-			buf_idx--;
+			handle_backspace();
 			redraw_screen();
 			continue;	
 		}
-		//printable key
-		if(buf_idx == buf_size)
+		if(key == CTRL('m'))
 		{
-			buffer = realloc(buffer,buf_size+10);
-			buf_size += 10;
+			insert_char('\0');
+			line_t *new = new_line();
+			new->prev = curr;
+			curr->next = new;
+			curr = new;
+			redraw_screen();
+			continue;
 		}
-		buffer[buf_idx++] = key;
+		//printable key
+	    insert_char(key);	
 		redraw_screen();
 	}
 	
