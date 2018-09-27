1db6a7d10e3c95100260c211e00e34d4f5a53d47
abc <abc@example.com>
2018-09-27 20:44:58 +0530

    fubar snapshot: hitting enter in between a line splits it

--- text_editor.c
+++ text_editor.c
@@ -26,20 +26,16 @@ static void redraw_screen()
 	clear();
 	int y=0,x=0;
 	line_t *line = first;
-	while(line)
-	{
-		for(int i=0;i<line->usize;i++)
-		{
-			if(line->text[i]=='\0')
-			{	
+	while(line){
+		for(int i=0;i<line->usize;i++){
+			if(line->text[i]=='\0'){	
 				y = (y+1)%height;
 				x = 0;
 				mvprintw(y,x,"");
 				continue;
 			}	
 			mvprintw(y,x++,"%c",line->text[i]);
-			if(x==width)
-			{
+			if(x==width){
 				x=0;
 				y=(y+1)%height;
 			}
@@ -48,20 +44,21 @@ static void redraw_screen()
 	}
 }
 
-static line_t* create_line()
+static line_t* create_line(const char *str)
 {
 	line_t *new = (line_t*) malloc(sizeof(line_t));
 	new->usize = 0;
-	new->asize = 0; 
+	new->asize = strlen(str); 
 	new->text = (char*)malloc(new->asize);
+	strcpy(new->text,str);
+	new->usize = new->asize;
 	new->next = new->prev = NULL;
 	return new;
 }
 
 static void insert_char(char c)
 {
-	if(curr->usize == curr->asize)
-	{
+	if(curr->usize == curr->asize){
 		curr->asize +=1;
 		curr->text = realloc(curr->text,curr->asize);
 	}
@@ -69,14 +66,24 @@ static void insert_char(char c)
 	x = (x+1)%width;
 }
 
+/*
+static void insert_str(line_t line,char *str)
+{
+	if(curr->asize-curr->usize < strlen(str)){
+		curr->asize += strlen(str);
+		curr->text = realloc(curr->text,curr->asize);
+	}
+	//strcpy(	
+			
+}*/
+
 static void  handle_backspace()
 {
 	line_t *line = curr;
 	while(line && line->usize==0)
 		line = line->prev;
-	if(line)
-	{
-		line->text[line->usize]='\0';
+	if(line){
+		//line->text[line->usize]='\0';
 		line->usize--;
 	}
 	redraw_screen();
@@ -84,11 +91,24 @@ static void  handle_backspace()
 
 static void handle_enter()
 {
+	line_t *new;
+	if(x < curr->usize)	{
+		new = create_line(curr->text+x);
+		curr->usize=x;
+	}
+	else {
+		new = create_line("");
+	}
 	insert_char('\0');
-	line_t *new = create_line();
-	new->prev = curr;
-	curr->next = new;
-	curr = new;
+
+	if(curr->next){
+		    
+	}
+	else {	
+		new->prev = curr;
+		curr->next = new;
+		curr = new;
+	}
 	y = (y+1)%height;
 	x = 0;
 	redraw_screen();
@@ -96,8 +116,7 @@ static void handle_enter()
 
 static void handle_keyup()
 {
-	if(curr->prev!=NULL)
-	{
+	if(curr->prev!=NULL) {
 		y--;
 		curr = curr->prev;	
 		x = curr->usize-1;
@@ -107,8 +126,7 @@ static void handle_keyup()
 
 static void handle_keydown()
 {
-	if(curr->next!=NULL)
-	{
+	if(curr->next!=NULL) {
 		y++;	
 		curr = curr->next;
 		x = curr->usize-1;
@@ -124,8 +142,7 @@ static void handle_keyleft()
 
 static void handle_keyright()
 {
-	if(x != curr->usize)
-	{
+	if(x != curr->usize){
 		x++;
 		move(y,x);
 	}	
@@ -137,41 +154,34 @@ int main()
 	initscr(); raw(); noecho(); nonl(); keypad(stdscr, TRUE);
 	setlocale(LC_ALL, ""); 
    	getmaxyx(stdscr, height, width);
-	first = create_line();
+	first = create_line("");
 	curr=first;
 	
-	while(1)
-	{
+	while(1){
 		key = wgetch(stdscr);
 		if(key==CTRL('Q'))
 			break;
-		if(key==127)//KEY_BACKSPACE)
-		{
+		if(key==127) {//KEY_BACKSPACE)
 			handle_backspace();
 			continue;	
 		}
-		if(key == CTRL('m'))
-		{
+		if(key == CTRL('m')){
 			handle_enter();
 			continue;
 		}
-		if(key == KEY_UP)
-		{
+		if(key == KEY_UP){
 			handle_keyup();
 			continue;
 		}
-		if(key == KEY_DOWN)
-		{
+		if(key == KEY_DOWN){
 			handle_keydown();
 			continue;
 		}
-		if(key == KEY_LEFT)
-		{
+		if(key == KEY_LEFT){
 			handle_keyleft();
 			continue;
 		}	
-		if(key == KEY_RIGHT)
-		{
+		if(key == KEY_RIGHT){
 			handle_keyright();
 			continue;
 		}
